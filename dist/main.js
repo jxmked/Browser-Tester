d19f55ec273dbbe5196ce5e49cd2935146c81631
jxmked <jamc015lol@gmail.com>
2022-09-25 18:02:49 +0800

    Thinking about design

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,5 @@
-"use strict";
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+});
 //# sourceMappingURL=main.js.map
